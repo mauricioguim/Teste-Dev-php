0f1966104c3270e949b045669e67b1ba27832489
mauricioguim <mauricioguimaraes@outlook.com>
2025-05-28 18:21:58 -0300

    feat: form request para validações de atributos de cliente e endereço

---
+++ app/Http/Requests/StoreClientRequest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CPFValidation;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'cpf' => [
+                'required',
+                'string',
+                'size:11',
+                'unique:clients',
+                new CPFValidation()
+            ],
+            'email' => [
+                'required',
+                'email',
+                'unique:clients'
+            ],
+            'phone' => [
+                'nullable',
+                'string',
+                'max:20'
+            ],
+            'address' => [
+                'required',
+                'array'
+            ],
+            'address.cep' => [
+                'required',
+                'digits:8'
+            ],
+            'address.street' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'address.neighborhood' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'address.city' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'address.state' => [
+                'required',
+                'string',
+                'max:100'
+            ],
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateClientRequest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CPFValidation;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'sometimes',
+                'string',
+                'max:255'
+            ],
+            'cpf' => [
+                'sometimes',
+                'string',
+                'size:11',
+                Rule::unique('clients')->ignore($this->client),
+                new CPFValidation(),
+            ],
+            'email' => [
+                'sometimes',
+                'email',
+                Rule::unique('clients')->ignore($this->client)
+            ],
+            'phone' => [
+                'nullable',
+                'string',
+                'max:20'
+            ],
+            'address' => [
+                'sometimes',
+                'array'
+            ],
+            'address.cep' => [
+                'required_with:address',
+                'digits:8'
+            ],
+            'address.street' => [
+                'required_with:address',
+                'string',
+                'max:255'
+            ],
+            'address.neighborhood' => [
+                'required_with:address',
+                'string',
+                'max:255'
+            ],
+            'address.city' => [
+                'required_with:address',
+                'string',
+                'max:255'
+            ],
+            'address.state' => [
+                'required_with:address',
+                'string',
+                'max:100'
+            ],
+        ];
+    }
+}
